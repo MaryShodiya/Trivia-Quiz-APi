0b581c04ed159bcc54c18dda234c60d6f30c1125
MaryShodiya <marysho57@gmail.com>
2022-06-07 00:06:30 +0100

    added comments

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+/*** The First Page Script ***/
+// a prompt for users to enter their name
 
-let enterName = document.querySelector('#enterName');
-enterName.addEventListener('click', getName);
+let enterName = document.querySelector('#enterName')
+enterName.addEventListener('click', getName)
 function getName() {
-    let userName= document.querySelector('input').value;
+    //read the value of entered name in the field and returns the username inserted in the welcome message
+    let userName= document.querySelector('input').value
     if (userName != null){
 document.getElementById("welcomePrompt").innerText ="Welcome " + userName + "! Get Ready to Play Diya Trivia";
 }
@@ -10,14 +13,16 @@ document.getElementById("welcomePrompt").innerText ="Welcome " + userName + "! G
 
 enterName.addEventListener('click', addButton)
 function addButton(){
-    const playBtn = document.querySelector('.playButton');
-    const createBtn = document.createElement('BUTTON');
-    const textOnBtn = document.createTextNode('play');
-    createBtn.appendChild(textOnBtn);
+    const playBtn = document.querySelector('.playButton')
+     //creates a button to navigate to  the next page of the application 
+    const createBtn = document.createElement('BUTTON')
+    const textOnBtn = document.createTextNode('play') //adds text-play to button
+    createBtn.appendChild(textOnBtn) //append text to button 
 
+    //onclick of the play button, user enters a new page
     playBtn.appendChild(createBtn).addEventListener('click', getNewPage)
   function getNewPage() {
-        location.href= "main.html";
+        location.href= "main.html"; 
     }
 }
 
--- js/main1.js
+++ js/main1.js
@@ -1,6 +1,8 @@
-let url = ''
-  let currentData = 0
-  let scores = 1
+let url = '' //url for api address
+  let currentData = 0 //
+  let scores = 1 //current score data
+
+  //button to reveal hidden div
    document.querySelector('#start').addEventListener('click' , revealDiv, getOutput) 
    function revealDiv(){
     let hidden = document.querySelector('.hidden')
@@ -12,52 +14,63 @@ let url = ''
     getOutput();
 }
        function getOutput(){
-      let easymode = document.querySelector('#easymode');
+           /* the different game mode options */
+      let easymode = document.querySelector('#easymode'); 
       let mediummode = document.querySelector('#mediummode');
       let hardmode = document.querySelector('#hardmode');
+
+      //a display error to be activated where a user does not select a game mode
       if ( (easymode.checked == true && mediummode.checked == true) || 
       (easymode.checked == true && hardmode.checked == true) || (hardmode.checked== true && mediummode.checked)){
         document.querySelector('#error').innerText= "Error! More than one value selected. Please select only one mode of play from the boxes"
-    }else if(easymode.checked == true){
+    }else if(easymode.checked == true){ //url for easy questions- diaplays the set of questions if easy mode is selected
           url = "https://opentdb.com/api.php?amount=48&difficulty=easy"
       }else if (mediummode.checked == true ) {
-          url = "https://opentdb.com/api.php?amount=48&difficulty=medium"
+          url = "https://opentdb.com/api.php?amount=48&difficulty=medium" //url for fairly difficult questions where medium mode is selected
       }else if(hardmode.checked == true){
-          url = "https://opentdb.com/api.php?amount=48&difficulty=hard"
+          url = "https://opentdb.com/api.php?amount=48&difficulty=hard" //url for very hard questions where hard mode is selected
       }else{
-          document.querySelector('#error').innerText= "Error! No Value Selected. Please select a mode of play"
+          document.querySelector('#error').innerText= "Error! No Value Selected. Please select a mode of play" //error message where a mode is not selected
       }
       fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-   console.log(data.results)
+   console.log(data.results) //collects all the results from the api data
 
+   //the answer options
+   //collects the correct answer data and incorrect answer
+   //sort through the answer so answers are not displayed in the same order
    let answers = data.results[currentData].incorrect_answers.concat(data.results[currentData].correct_answer).sort();
-   Array.from(answers).forEach((answer, index) => {
-       
+   
+   //loops through each answer to create a checkbox element
+   Array.from(answers).forEach((answer, index) => { 
+       // creates checkbox
        const checkbox = document.createElement('input')
-       const id = `myCheckbox-${index}`
-       checkbox.type = 'checkbox'
-       checkbox.id= id
-       checkbox.value = answer
-     const label = document.createElement('label')
+       const id = `myCheckbox-${index}` //unique id for the checkbox
+       checkbox.type = 'checkbox' //type of the checkbox
+       checkbox.id= id //reassign the checkbox id
+       checkbox.value = answer //assign the answer as value for the checkbox
+     const label = document.createElement('label') //creates label
      label.htmlFor= id;
-    label.appendChild(document.createTextNode(answer))
+    label.appendChild(document.createTextNode(answer)) //append checkbox to label
    document.querySelector('#answers').appendChild(checkbox)
-document.querySelector('#answers').appendChild(label)
+document.querySelector('#answers').appendChild(label) //append label to answers id attribute in html
    })
    
-   document.querySelector('#category').innerText = data.results[currentData].category;
- document.querySelector('#question').innerText = data.results[currentData].question
+   document.querySelector('#category').innerText = data.results[currentData].category; //assign catagory in the api data to category id attribute in html
+ document.querySelector('#question').innerText = data.results[currentData].question    //assign question in the api data to question id attribute in html
  
-    
+    //event listener for submit answer button
 document.querySelector('#submitAnswer').addEventListener('click', submitAnswer)
        function submitAnswer() {
-      let firstanswer = document.getElementById('myCheckbox-0')
+
+        //assign unique ids to each checkbox
+      let firstanswer = document.getElementById('myCheckbox-0') 
       let secondanswer = document.getElementById('myCheckbox-1')
       let thirdanswer = document.getElementById('myCheckbox-2')
       let fourthanswer = document.getElementById('myCheckbox-3')
        
+      //conditionals to tell the correct answer from the checkboxes selected
  if ( (firstanswer.checked && firstanswer.value === data.results[currentData].correct_answer) || (secondanswer.checked && secondanswer.value === data.results[currentData].correct_answer) 
       || (thirdanswer.checked && thirdanswer.value === data.results[currentData].correct_answer) || (fourthanswer.checked && fourthanswer.value === data.results[currentData].correct_answer) ){
         document.querySelector('#result').innerText= "CORRECT!"
@@ -67,6 +80,7 @@ document.querySelector('#submitAnswer').addEventListener('click', submitAnswer)
         
        }
     }
+    //clue options to tell the user the correct answer
      document.querySelector('#clue').addEventListener('click', showAnswer)
          function showAnswer(){
              document.querySelector('#warning').innerText= "Warning! You will lose a mark"
@@ -82,7 +96,7 @@ document.querySelector('#submitAnswer').addEventListener('click', submitAnswer)
           
        }
 
-
+//event listener to navigate to the next question
        document.querySelector('#next').addEventListener('click', getNewPage)
        function getNewPage() {
             
@@ -93,7 +107,7 @@ document.querySelector('#submitAnswer').addEventListener('click', submitAnswer)
            currentData++
            getOutput();  
        }
-
+//event listener for restart button
        document.getElementById('restart').addEventListener('click', startAgain)
        function startAgain() {
         document.querySelector('#question').innerText=''
@@ -103,7 +117,7 @@ document.querySelector('#submitAnswer').addEventListener('click', submitAnswer)
         document.querySelector('#category').innerText=''
 
        }
-        
+        //warning button to alert user of the danger of using  the clue button
         document.getElementById('warnbutton').addEventListener('click', hideAlert)
         function hideAlert() {
             document.querySelector('#warning').innerText= ''
